421bb2938a93683e1b29f0ad7f37a6db5f6dddad
Faith Daisy Muchira <faidamuchira@gmail.com>
2026-04-16 13:19:17 +0100

    Create database setup script

---
+++ sql/01_create_database.sql
@@ -0,0 +1,10 @@
+-- ============================================
+-- File: 01_create_database.sql
+-- Description: Creates the database for the eSports system
+-- ============================================
+
+-- create the database
+CREATE DATABASE IF NOT EXISTS eSports_db; -- to prevent errors if the table already exists
+
+-- use the database
+USE eSports_db;
